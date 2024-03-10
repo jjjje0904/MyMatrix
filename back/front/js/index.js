858c8bbbaa98869eded54f263e0577a01aa0f2b5
jjjje0904 <rosa1098@naver.com>
2024-03-10 23:48:13 +0900

    try

--- back/front/js/index.js
+++ back/front/js/index.js
@@ -1,9 +1,9 @@
 setHeader();
 
 async function setHeader(){
-    //로컬스토리지에 토큰 존재여부 검사
+    //�슖�돦裕뉛쭚袁ъ삕占쎈츩�뜝�럥苑낉옙逾놂옙�뵃�삕�뜝�룞�삕�굢占� �뜝�럥苑끻뜝�럡荑� �댖怨뺣샍占쎌궨�뜝�럥�뿰占쎄껀�뜝占� �뇦猿볦삕�뜝�럡�뀬
     const token = localStorage.getItem("x-access-token");
-    //토큰이 없다면 signed에 hidden 클래스 붙이기
+    //�뜝�럥苑끻뜝�럡荑껃뜝�럩逾� �뜝�럥�뵪�뜝�럥堉꾤춯濡녹삕 signed�뜝�럥�뱺 hidden �뜝�럡源삣뜝�럩�굥�뜝�럥裕� 占쎄껀占쎈듌占쎈턄�뼨��먯삕
     if(!token){
         const signed = document.querySelector(".signed");
         signed.classList.add("hidden");
@@ -19,21 +19,21 @@ async function setHeader(){
     const res = await axios(config);
 
     if(res.data.code !=200){
-        console.log("잘못된 토큰입니다.");
+        console.log("�뜝�럩�굥嶺뚮쪇沅좑쭕占� �뜝�럥苑끻뜝�럡荑껃뜝�럩肉��뜝�럥鍮띶뜝�럥堉�.");
         return;
     }
 
     const nickname = res.data.result.nickname;
     const spanNickname = document.querySelector("span.nickname");
     spanNickname.innerText = nickname;
-    //토큰이 있다면 unsigned에 hidden 클래스 붙이기
+    //�뜝�럥苑끻뜝�럡荑껃뜝�럩逾� �뜝�럩肉녑뜝�럥堉꾤춯濡녹삕 unsigned�뜝�럥�뱺 hidden �뜝�럡源삣뜝�럩�굥�뜝�럥裕� 占쎄껀占쎈듌占쎈턄�뼨��먯삕
     const unsigned =document.querySelector(".unsigned");
     unsigned.classList.add("hidden");
 
 }
 
 
-//########### 로그아웃 기능
+//########### �슖�돦裕꾬옙�쟽�뜝�럥�닡�뜝�럩�쐨 �뼨轅명�∽옙裕�
 const buttonSignout = document.getElementById("sign-out");
 buttonSignout.addEventListener("click",signout);
 
